25615737a447e65647712f5a7d6ef9e37bfe53fb
Victor Rios <victor_github@outlook.com>
2017-11-08 23:55:58 +0100

    localhost removed for deployment.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
